cb39aa632baa56d35575c132b04d9483b3124a10
wpmjones@gmail.com <8751ImpalaMustang!>
2019-07-31 14:10:27 -0400

    testing quart

--- temp.py
+++ temp.py
@@ -1,8 +1,60 @@
-from quart import Quart
+from quart import Quart, redirect, url_for, request, render_template, flash
+from wtforms import Form, TextAreaField, SelectField, validators
+from twilio.rest import Client
+from config import settings
 
 app = Quart(__name__)
 
+# Set up Twilio
+twilio = Client(settings['twilio']['sid'], settings['twilio']['token'])
 
-@app.route("/")
-async def hello():
-    return "<h1 style='color:blue'>Welcome to Salvation Army Texting!</h1>"
+# This should come from the database
+choices = [("students", "Students"),
+           ("staff", "Staff"),
+           ("band", "Band"),
+           ("songsters", "Songsters"),
+           ("womens_bible", "Women's Bible Study"),
+           ("home_league", "Home League"),
+           ("testers", "Debug"),
+           ]
+
+
+class HomePage:
+    @app.route("/")
+    async def index():
+        return redirect(url_for("sendmsg"))
+
+
+class HomeForm(Form):
+    group = SelectField("Recipients:", choices=choices)
+    msg = TextAreaField("Message:", validators=[validators.required()])
+
+    @app.route("/sendmsg", methods=["GET", "POST"])
+    def sendmsg():
+        # if current_user.is_authenticated:
+        form = HomeForm(request.form)
+        print(form.errors)
+        # profile_pic = current_user.profile_pic
+        if request.method == "POST":
+            if form.validate():
+                group = request.form['group']
+                message = request.form['msg']
+                # recipients, phone_nums = get_recipients(group)
+                recipients = ["Patrick", "Jennifer"]
+                phone_nums = ["+17274632720", "+17274631360"]
+                # TODO move messages to separate function
+                # TODO log message to database
+                for phone_num in phone_nums:
+                    twilio_msg = twilio.messages.create(to=phone_num,
+                                                        from_=settings['twilio']['phone_num'],
+                                                        body=message
+                                                        )
+                    print(twilio_msg)
+                flash(f"Message sent to: {', '.join(recipients)}")
+            else:
+                flash("Error: All form fields are required.")
+        # TODO figure out how to set up options in sendmgs.html from Receipients.columns()
+        return render_template("sendmsg.html", form=form)  # , profile_pic=profile_pic)
+        # else:
+        #     return ("<h2>St. Petersburg Text App</h2>"
+        #             "<a class='btn btn-default' href='/login' role='button'>Google Login</a>")
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from temp import app
-
-if __name__ == "__main__":
-    app.run()

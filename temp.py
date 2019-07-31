92589aee841b3d469758727d1e521543a77493eb
wpmjones@gmail.com <8751ImpalaMustang!>
2019-07-31 13:35:06 -0400

    testing quart

--- temp.py
+++ temp.py
@@ -5,8 +5,4 @@ app = Quart(__name__)
 
 @app.route("/")
 async def hello():
-    return "<h1 style='color:blue'>Welcome to Salvation Army Texting</h1>"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    return "<h1 style='color:blue'>Welcome to Salvation Army Texting!</h1>"

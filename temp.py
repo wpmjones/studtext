b042e74b7b54354ddd7d5e35756110304a591163
W. Patrick Jones <wpmjones@gmail.com>
2019-07-30 23:22:01 -0400

    testing gunicorn

--- temp.py
+++ temp.py
@@ -1,26 +1,11 @@
-import requests
-import json
-import sqlite3
-from flask import Flask, render_template, flash, request, url_for, redirect
-from flask_login import LoginManager, current_user, login_required, login_user, logout_user
-from oauthlib.oauth2 import WebApplicationClient
-from wtforms import Form, TextAreaField, SelectField, validators
-from twilio.rest import Client
-from db import init_db_command, User, Receipients
-from config import settings
+from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config["SECRET_KEY"] = settings['flask']['key']
-
-# Flask-Login
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Welcome to Salvation Army Texting</h1>"
 
 
 if __name__ == "__main__":

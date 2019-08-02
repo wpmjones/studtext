32068ec4e9c82a3181d9ee4c40e1b8b4877419ab
W. Patrick Jones <wpmjones@gmail.com>
2019-08-01 20:57:49 -0400

    hard code redirect uri to maintain https

--- temp.py
+++ temp.py
@@ -95,7 +95,7 @@ async def login():
     # Use library to construct the request for Google login and provide
     # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(authorization_endpoint,
-                                             redirect_uri=request.base_url + "/callback",
+                                             redirect_uri="https://satext.com/login/callback",
                                              scope=["openid", "email", "profile"])
     return redirect(request_uri)
 

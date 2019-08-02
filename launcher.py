a14436ceef2b9ca24d20bc9cd656b2ebae7c0605
W. Patrick Jones <wpmjones@gmail.com>
2019-08-01 20:44:09 -0400

    back to basks with launcher

--- launcher.py
+++ launcher.py
@@ -1,14 +1,32 @@
 import quart.flask_patch
 
+import requests
 from secrets import compare_digest
+from loguru import logger
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user, UserMixin
 from quart import Quart, redirect, request, url_for
+from oauthlib.oauth2 import WebApplicationClient
 from config import settings
 
 app = Quart(__name__)
 app.secret_key = settings['flask']['key']
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+# Google Configuration
+google_client_id = settings['google']['id']
+google_client_secret = settings['google']['secret']
+google_discovery_url = "https://accounts.google.com/.well-known/openid-configuration"
+
+
+# Get Google Provider
+def get_google_provider_cfg():
+    logger.debug("start get_google_provider_cfg")
+    return requests.get(google_discovery_url).json()
+
+
+# OAuth2 client setup
+client = WebApplicationClient(google_client_id)
 
 # Rather than storing passwords in plaintext, use something like
 # bcrypt or similar to store the password hash.
@@ -30,6 +48,12 @@ def user_loader(username):
 
 @app.route('/', methods=['GET', 'POST'])
 async def login():
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    request_uri = client.prepare_request_uri(authorization_endpoint,
+                                             redirect_uri=request.base_url + "/callback",
+                                             scope=["openid", "email", "profile"])
+    logger.debug(request_uri)
     if request.method == 'GET':
         return '''
                <form method='POST'>

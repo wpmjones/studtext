7c0bc62336354222a0b800a565dde2812234024a
W. Patrick Jones <wpmjones@gmail.com>
2019-08-10 15:26:01 -0400

    move utility functions to utils.py

---
+++ utils.py
@@ -0,0 +1,52 @@
+import requests
+from config import settings
+from db import Messages
+
+
+def discord_log(message):
+    payload = {
+        "title": f"{message.record['name']}:{message.record['module']}:{message.record['line']}",
+        "fields": {
+            {"name": message.record["level"], "value": message.record["message"], "inline": False}
+        },
+        "footer": {
+            "text": message.record["time"]
+        }
+    }
+    requests.post(settings["discord"]["webhook"], data=payload)
+    if message.record["exception"]:
+        content = f"python\n{message.record['exception']}"
+        send_text(settings["discord"]["webhook"], content, block=1)
+
+
+def send_text(webhook, text, block=None):
+    """ Sends text ot channel, splitting if necessary """
+    if len(text) < 1993:
+        if block:
+            requests.post(webhook, f"```{text}```")
+        else:
+            requests.post(webhook, text)
+    else:
+        coll = ""
+        for line in text.splitlines(keepends=True):
+            if len(coll) + len(line) > 1993:
+                # if collecting is going to be too long, send  what you have so far
+                if block:
+                    requests.post(webhook, f"```{coll}```")
+                else:
+                    requests.post(webhook, coll)
+                coll = ""
+            coll += line
+        if block:
+            requests.post(webhook, f"```{coll}```")
+        else:
+            requests.post(webhook, coll)
+
+
+def welcome_message(twilio, recipient_id, name, phone):
+    body = (f"Welcome {name}! You've been added to a group for Salvation Army text messages. "
+            f"If you have questions, talk to your corps officers. Text 'STOP' to cancel messages.")
+    twilio_msg = twilio.messages.create(to=phone,
+                                        from_=settings["twilio"]["phone_num"],
+                                        body=body)
+    Messages.add_message(twilio_msg.sid, "WELCOME", recipient_id, 0, body)
